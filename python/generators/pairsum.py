9d4cb82aa0ff1b5f26181140272ab8e14291503a
terrathedeveloper <ttdevelopedit@gmail.com>
2022-06-20 12:35:22 -0500

    init commit

--- python/generators/pairsum.py
+++ python/generators/pairsum.py
@@ -13,7 +13,7 @@ def getPairBySum(nums,targetSum):
     for i, _ in enumerate(nums[:-1]):  #use enumerate to create an indexed pair list and iterate
                                         # through that list to get the sums of the consecutive numbers
         if nums[i] + nums[i+1]==targetSum:
-            yield "({}, {})".format(nums[i], nums[i+1])
+            yield "({num1}, {num2})".format(num1=nums[i], num2=nums[i+1])
         elif list[i-1] and nums[i] + nums[i-1]==targetSum: #check to make sure the previous elment exists 
                                                     #before checking to see if the elements equal the sum
             yield "({num1}, {num2})".format(num1=nums[i], num2=nums[i-1])
